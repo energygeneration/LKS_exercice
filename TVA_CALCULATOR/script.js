2881bfe637266545026797782347ef608632b802
LOUMON-Koffi-Samuel <lkoffisamuel@gmail.com>
2022-06-14 19:07:36 +0000

    deplacement des variables dans la fonction

--- TVA_CALCULATOR/script.js
+++ TVA_CALCULATOR/script.js
@@ -1,12 +1,13 @@
-const pourcentageTva = document.getElementById('TVA').value / 100;
-console.log(pourcentageTva);
-const prixSansTva = document.getElementById('HT').value;
-console.log(prixSansTva);
+
 
 function check(){
+    let pourcentageTva = document.getElementById('TVA').value / 100;
+console.log(pourcentageTva);
+let prixSansTva = document.getElementById('HT').value;
+console.log(prixSansTva);
 const montantDeLaTva = pourcentageTva * prixSansTva;
 console.log( montantDeLaTva);
-if(montantDeLaTva > 0){
+if(montantDeLaTva < 0){
     alert('Montant inférieur à 0');
 }
 else{
